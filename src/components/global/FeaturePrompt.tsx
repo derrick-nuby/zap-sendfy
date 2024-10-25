3949939a2c88563f3ac448c8c1ef5f0b99d73c86
Derrick-Nuby <iradukundaderrick7@gmail.com>
2024-10-25 10:15:16 +0200

    feature prompt size done

--- src/components/global/FeaturePrompt.tsx
+++ src/components/global/FeaturePrompt.tsx
@@ -1,9 +1,10 @@
-import { Link, ArrowRightIcon } from 'lucide-react';
+import { ArrowRightIcon } from 'lucide-react';
 import React from 'react';
 import { Button } from '../ui/button';
 import { LampContainer } from '../ui/lamp';
 import AnimationContainer from './animation-container';
 import MaxWidthWrapper from './max-width-wrapper';
+import Link from 'next/link';
 
 interface FeaturePromptProps {
   headingText: string;
@@ -13,12 +14,12 @@ const FeaturePrompt: React.FC<FeaturePromptProps> = ({ headingText, descriptionT
   return (
     <MaxWidthWrapper className="pt-20">
       <AnimationContainer delay={0.4} className="w-full">
-        <LampContainer className="max-w-2xl mx-auto">
+        <LampContainer className="max-w-7xl mx-auto">
           <div className="flex flex-col items-center justify-center relative w-full text-center">
             <h2 className="bg-gradient-to-br from-neutral-300 to-neutral-500 py-4 bg-clip-text text-center text-3xl font-semibold font-heading tracking-tight text-transparent md:text-5xl mt-8">
               {headingText}
             </h2>
-            <p className="text-muted-foreground mt-6 max-w-2xl mx-auto text-base md:text-lg">
+            <p className="text-muted-foreground mt-6 max-w-3xl mx-auto text-base md:text-lg">
               {descriptionText}
             </p>
             <div className="mt-6">
